0a8e53f38383e3c7d7a6ffb4e5aabc10c8a87a1b
SmausJakub <jakub.smaus@seznam.cz>
2017-11-17 14:56:46 +0100

    Změněna pozice dokumentů a přidání složky pro testovací textové soubory Main předělán, aby získával jako argument soubor.

--- src/cz/zcu/kiv/fjp/Main.java
+++ src/cz/zcu/kiv/fjp/Main.java
@@ -3,21 +3,33 @@ package cz.zcu.kiv.fjp;
 
 
 import cz.zcu.kiv.fjp.compiler.SymbolMap;
-import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static String inputFile;
 
-        Pascal0LikeLexer lexer = new Pascal0LikeLexer(new ANTLRInputStream("begin end."));
-        Pascal0LikeParser parser = new Pascal0LikeParser(new CommonTokenStream(lexer));
-        ParseTree tree = parser.program();
-        Pascal0LikeBaseVisitor visitor = new Pascal0LikeBaseVisitor();
-        visitor.visit(tree);
+    public static void main(String[] args) throws Exception{
 
-        System.out.println(SymbolMap.getSymbolMap().toString());
+        if(args.length > 0) {
+
+            inputFile = args[0];
+            CharStream inputStream = CharStreams.fromFileName(inputFile);
+
+            Pascal0LikeLexer lexer = new Pascal0LikeLexer(inputStream);
+            Pascal0LikeParser parser = new Pascal0LikeParser(new CommonTokenStream(lexer));
+            ParseTree tree = parser.program();
+            Pascal0LikeBaseVisitor visitor = new Pascal0LikeBaseVisitor();
+            visitor.visit(tree);
+
+            System.out.println(SymbolMap.getSymbolMap().toString());
+
+        }else {
+            System.out.println("Nazev programu nebyl zadan. Zadejte prosim nazev souboru");
+        }
 
     }
 
