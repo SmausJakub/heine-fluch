55f2405e0a29930363544fecfb8aef9c347c0a68
Kossyr <marty.kruz@seznam.cz>
2017-11-18 12:34:58 +0100

    reseni vstupu v main

--- src/cz/zcu/kiv/fjp/Main.java
+++ src/cz/zcu/kiv/fjp/Main.java
@@ -11,22 +11,59 @@ public class Main {
 
     private static String inputFile;
 
+    private static void usage() {
+
+        System.out.println("PascaL0-like prekladac a interpret");
+        System.out.println("Pouziti:");
+        System.out.println("-h : zobraz napovedu");
+        System.out.println("-c [soubor] : preloz zdrojovy soubor jazyka PascaL0-like");
+        System.out.println("-i [soubor] : interpretuj prelozeny soubor jazyka PascaL0-like");
+
+    }
+
     public static void main(String[] args) throws Exception{
 
         if(args.length > 0) {
 
-            inputFile = args[0];
-            CharStream inputStream = CharStreams.fromFileName(inputFile);
+            String command = args[0];
 
-            Pascal0LikeLexer lexer = new Pascal0LikeLexer(inputStream);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            Pascal0LikeParser parser = new Pascal0LikeParser(tokens);
-            ParseTree tree = parser.program();
-            Visitor visitor = new Visitor();
-            visitor.visit(tree);
+            if (command.equalsIgnoreCase("-h") == true) {
+
+                // help
+                usage();
+            } else if (command.equalsIgnoreCase("-c")) {
+
+                // compiler
+
+                if (args.length > 1) {
+
+                    inputFile = args[1];
+                    CharStream inputStream = CharStreams.fromFileName(inputFile);
+
+                    Pascal0LikeLexer lexer = new Pascal0LikeLexer(inputStream);
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+                    Pascal0LikeParser parser = new Pascal0LikeParser(tokens);
+                    ParseTree tree = parser.program();
+                    Visitor visitor = new Visitor();
+                    visitor.visit(tree);
+
+                } else {
+                    System.out.println("Nezadan nazev souboru.");
+                }
+
+            } else if (command.equalsIgnoreCase("-l")) {
+
+                // interpret
+            } else {
+                System.out.println("Neznamy argument. Vypisuji pouziti.");
+                usage();
+            }
+
+
+
 
         }else {
-            System.out.println("Nazev programu nebyl zadan. Zadejte prosim nazev souboru");
+            usage();
         }
 
     }
