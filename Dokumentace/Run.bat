12d0ff030ba25fa6f5b601b865f7856e802c5863
SmausJakub <jakub.smaus@seznam.cz>
2017-12-14 15:51:27 +0100

    Změna ve struktuře dokumentace Přidány soubory v latexu pro tabulky HOTFIX

--- Dokumentace/Run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Pokud probehne bez chyb, vytvori se soubor Dokumentace.pdf.
-echo V pripade chyb spust rucne "pdflatex Dokumentace.tex" a oprav pripadne chyby.
-del Dokumentace.pdf 1>nul 2>nul
-pdflatex Dokumentace.tex 1>nul
-pdflatex Dokumentace.tex 1>nul
